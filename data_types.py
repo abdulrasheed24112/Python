c616d2b47196a356b0224c8c44c1099656125df9
abdulrasheed24112 <ar85435@gmail.com>
2021-03-21 16:24:03 +0500

    integer and booleans

--- data_types.py
+++ data_types.py
@@ -1,2 +1,30 @@
 print(15) #positive integer number 
 print(-10) #negitive integer number 
+a= 10 #intilize value to variable(positive integer number) 
+b=-20 #negitive integer number
+
+
+number = 12233
+print(number)
+number=-2122
+print(number)#assign negitive value
+
+#float numbers
+floatNumber=1.232
+print(floatNumber)
+print(-1.233)# negitive float number
+
+#compelx numbers 
+# python provides complex() function is used to create complex numbers it takes two parameters real and imaginary e.g(10 + 20j)
+comp_num_1 = 10
+comp_num_2 = 20
+print("Complex number is ",complex(comp_num_1,comp_num_2))
+
+comp_1 = complex(3, 2)
+comp_2 = complex(4, 0)
+print(comp_1)
+print(comp_2)
+
+#Booleans
+num_boolean=False
+print(num_boolean)
